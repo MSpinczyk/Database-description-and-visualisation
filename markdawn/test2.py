d328143e47b656ef5992c3cf58a7ffd25f6d3080
jmrozek <janmrozek17@gmail.com>
2023-12-13 22:09:19 +0100

    sql->dbml

--- markdawn/test2.py
+++ markdawn/test2.py
@@ -3,7 +3,7 @@ import subprocess
 '''GETTING SQL SYNTAX FROM DATABase'''
 
 # Command to run
-command = 'pg_dump --dbname=postgresql://postgres:jmrozek@127.0.0.1:5432/Project1 -s > dump.txt'
+command = 'pg_dump --dbname=postgresql://jmrozek:furm3w3sj5s7@lab.kis.agh.edu.pl:1500/jmrozek -s  --schema=project > dump.sql'
 
 try:
     result = subprocess.run(command, shell=True)
@@ -11,3 +11,18 @@ try:
 except subprocess.CalledProcessError as e:
     # Handle any errors that occurred during the command execution
     print("Error:", e)
+
+
+''' getting dbml model from sql'''
+
+command = 'sql2dbml dump.sql --postgres'
+
+try:
+    with open('dbml_structure.txt', 'w') as file:
+        result = subprocess.run(command, shell=True, stdout=file)
+
+
+except subprocess.CalledProcessError as e:
+    # Handle any errors that occurred during the command execution
+    print("Error:", e)
+
